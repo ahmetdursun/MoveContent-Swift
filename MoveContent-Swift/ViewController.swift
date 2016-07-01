37c63389068414343986a86b773a5a7ce58ddc19
Ahmet Dursun <ahmetdursun@gmail.com>
2016-07-01 04:02:57 +0300

    Move content implementation

--- MoveContent-Swift/ViewController.swift
+++ MoveContent-Swift/ViewController.swift
@@ -10,14 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLayoutConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tappedAction(_:)))
+        mainView.addGestureRecognizer(tap)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(self.keyboardNotification(_:)),
+                                                         name: UIKeyboardWillChangeFrameNotification,
+                                                         object: nil)
+    }
+    
+    //Remove keyboard
+    func tappedAction(gestureRecognizer:UIGestureRecognizer) {
+        mainView.endEditing(true)
+    }
+    
+    //Move content by height layout constraint. Change constraint according to keyboard size.
+    func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue()
+            let duration:NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.unsignedLongValue ?? UIViewAnimationOptions.CurveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if endFrame?.origin.y >= UIScreen.mainScreen().bounds.size.height {
+                self.heightLayoutConstraint?.constant = 0.0
+            } else {
+                self.heightLayoutConstraint?.constant = endFrame?.size.height ?? 0.0
+            }
+            UIView.animateWithDuration(duration,
+                                       delay: NSTimeInterval(0),
+                                       options: animationCurve,
+                                       animations: { self.view.layoutIfNeeded() },
+                                       completion: nil)
+        }
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
